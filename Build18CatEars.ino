7e4cf89587eb4a89e9108cdcc57ffb22784cf417
norawan <nyw@andrew.cmu.edu>
2023-01-20 15:00:54 -0500

    adapting example code

--- Build18CatEars.ino
+++ Build18CatEars.ino
@@ -15,6 +15,8 @@
 
 #define BAUDRATE 115200
 
+#define ATTN_THRESHOLD 0.60
+
 // Create servo objects for each servo
 Servo l_pan;
 Servo l_tilt;
@@ -37,25 +39,37 @@ void setup() {
   r_pan.attach(R_PAN_PIN);
   r_tilt.attach(R_TILT_PIN);
 
+  // Initialize servo positions
+  l_tilt.write(90);
+  l_pan.write(90);
+  r_tilt.write(90);
+  r_pan.write(90);
+
+  l_tilt_pos = 90;
+  l_pan_pos = 90;
+  r_tilt_pos = 90;
+  r_pan_pos = 90;
+
   // Begin Serial 
   Serial.begin(BAUDRATE);
-  Serial.print("hi");
+  Serial.print("Connecting to serial port...");
   Serial.print("\n");
 }
 
 void loop() {
 
   // Data received from MuseLSL
-  if(Serial.available()){
-    Serial.print("here");
+  if (Serial.available()){
+    Serial.print("Data received: ");
     String data = Serial.readStringUntil('\n');
     Serial.print(data);
     Serial.print("\n");
-    
-    // l_tilt.write(90);
-    // l_pan.write(90);
-    // delay(2000);
-    // wiggleEars(3);
+
+    float val = data.toFloat();
+    if (val > ATTN_THRESHOLD) {
+      wiggleEars(3);
+    }
+    delay(100);
   }
 }
 
@@ -66,10 +80,10 @@ void loop() {
  */
 void wiggleEars(int count) {
   for (int i = 0; i < count; i++) {
-    l_tilt.write(150);
-    l_pan.write(90);
+    l_tilt.write(100);
+    l_pan.write(50);
     delay(100);
-    l_tilt.write(50);
+    l_tilt.write(80);
     l_pan.write(90);
     delay(100);
   }
